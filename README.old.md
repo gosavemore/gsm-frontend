40417f154b41db5fd2bcc6edaf71a8174ea8a014
denmercs <denmercs@gmail.com>
2020-10-19 21:12:22 -0500

    Setting up redux boilerplate, Implementing registration login component

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# frontend
+front end repo for gosavemore.com business
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <h1>Go Save More</h1>
+    </div>
+  );
+}
+
+export default App;
