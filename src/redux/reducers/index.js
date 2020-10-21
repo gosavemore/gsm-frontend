af179ef24364f3c4fb368a9eef79ef7fdc7e2f51
Sean Doyle <sdoylelambda@gmail.com>
2020-10-21 18:12:24 -0400

    Products redux complete for get all products.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
 import userReducer from './userReducer'
 import authReducer from './authReducer'
-import productReducer from './productsReducer'
+import productsReducer from './productsReducer'
 
 export default combineReducers({
   user: userReducer,
   auth: authReducer,
-  products: productReducer,
+  products: productsReducer,
 })
