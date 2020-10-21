ad4c8c28d22327d904f5381882f45321a0ff9cce
denmercs <denmercs@gmail.com>
2020-10-20 20:04:22 -0500

    Login form sends new user data to BE.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Register2 from './components/Resister2'
+import Register2 from "./components/Resister2";
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
       <h1>Go Save More</h1>
-      <Register />
+      {/* <Register /> */}
       <div>
         <Register2 />
-        </div>
+      </div>
     </div>
   );
 }
--- src/components/Resister2.js
+++ src/components/Resister2.js
@@ -1,106 +1,68 @@
-// import React from 'react';
-// import { useForm } from 'react-hook-form';
-// import { registerUser } from "../redux/actions/userActions";
-
-// export default function Register2() {
-//   const { register, handleSubmit, errors } = useForm();
-//   const onSubmit = data => console.log(data);
-//   console.log(errors);
-  
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <input type="text" placeholder="First name" name="First name" ref={register({required: true, maxLength: 80})} />
-//       <input type="text" placeholder="Last name" name="Last name" ref={register({required: true, maxLength: 100})} />
-//       <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
-//       <input type="tel" placeholder="Mobile number" name="Mobile number" ref={register({required: true, minLength: 6, maxLength: 12})} />
-     
-//       <input type="submit" />
-//     </form>
-//   );
-// }
-
-
-
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useSelector, useDispatch } from "react-redux";
 import { registerUser } from "../redux/actions/userActions";
 
 export default function Register2() {
   const { register, handleSubmit, errors } = useForm();
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   const onSubmit = (data) => {
-    console.log('data ::::', data)
-    
-  }
+    dispatch(registerUser(data));
+  };
   console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-       <input
+      <label>User Name</label>
+      <input
         type="text"
         placeholder="Username"
         name="username"
-    
+        ref={register}
       />
+      <label>Email</label>
+      <input type="text" placeholder="Email" name="email" ref={register} />
+      <label>Password</label>
       <input
-        type="text"
-        placeholder="Email"
-        name="email"
-      
-      />
-          <input
         type="text"
         placeholder="password"
         name="password"
-       
+        ref={register}
       />
+      <label>First Name</label>
       <input
         type="text"
         placeholder="First Name"
         name="firstName"
-       
+        ref={register}
       />
+      <label>Last Name</label>
       <input
         type="text"
         placeholder="Last Name"
         name="lastName"
-     
+        ref={register}
       />
+      <label>House Number</label>
       <input
         type="text"
         placeholder="House Number"
         name="houseNumber"
-       
+        ref={register}
       />
-          <input
+      <label>Street name</label>
+      <input
         type="text"
         placeholder="Street Name"
         name="streetName"
-       
+        ref={register}
       />
-           <input
-        type="text"
-        placeholder="City"
-        name="city"
-        
-      />
-             <input
-        type="text"
-        placeholder="Zip"
-        name="zip"
-       
-      />
-                <input
-        type="text"
-        placeholder="State"
-        name="state"
-       
-      />
-                  <input
-        type="text"
-        placeholder="Country"
-        name="country"
-       
-      />
+      <input type="text" placeholder="City" name="city" ref={register} />
+      <input type="text" placeholder="Zip" name="zip" ref={register} />
+      <input type="text" placeholder="State" name="state" ref={register} />
+      <input type="text" placeholder="Country" name="country" ref={register} />
 
       <input type="submit" />
     </form>
