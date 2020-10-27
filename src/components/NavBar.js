dfeafcf7bc8c44cf16a41556dcfc657d92fc9f13
Sean Doyle <sdoylelambda@gmail.com>
2020-10-27 14:06:47 -0400

    Materize css setup with nav and basic styling completed.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,32 +5,34 @@ const NavBar = () => {
   return (
     <div className='navbar'>
       <nav>
-        <div>
-          <Link className='navLink home' to='/home'>
-            Home
-          </Link>
-        </div>
-        <div>
-          <Link className='navLink searchBar' to='/searchBar'>
-            MAKE THIS A SEARCH BAR
-          </Link>
-        </div>
-        <div>
-          <Link className='navLink register' to='/register'>
-            Register New User
-          </Link>
-        </div>
-        <div>
-          <Link className='navLink login' to='/login'>
-            Login Existing User
-          </Link>
-          <Link className='navLink track' to='/tracking'>
-            Track Order
-          </Link>
-          <Link className='navLink cart' to='/cart'>
-            Cart
-          </Link>
-        </div>
+        <ul id='nav-mobile' class='right hide-on-med-and-down'>
+          <li>
+            <Link className='navLink home' to='/home'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className='navLink searchBar' to='/searchBar'>
+              MAKE THIS A SEARCH BAR
+            </Link>
+          </li>
+          <li>
+            <Link className='navLink register' to='/register'>
+              Register New User
+            </Link>
+          </li>
+          <li>
+            <Link className='navLink login' to='/login'>
+              Login Existing User
+            </Link>
+            <Link className='navLink track' to='/tracking'>
+              Track Order
+            </Link>
+            <Link className='navLink cart' to='/cart'>
+              Cart
+            </Link>
+          </li>
+        </ul>
       </nav>
     </div>
   )
