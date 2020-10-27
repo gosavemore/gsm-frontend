a0cbce679c9c85590e714eb8dbb3940e03885fe8
Sean Doyle <sdoylelambda@gmail.com>
2020-10-27 18:42:14 -0400

    Styling for add product and login components.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
-import { loginUser } from "../redux/actions/authActions";
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../redux/actions/authActions'
 
 const Login = () => {
-  const { register, handleSubmit, errors } = useForm();
-  const dispatch = useDispatch();
+  const { register, handleSubmit, errors } = useForm()
+  const dispatch = useDispatch()
 
   const onSubmit = (data) => {
-    dispatch(loginUser(data));
-  };
-  console.log(errors);
+    dispatch(loginUser(data))
+  }
+  console.log(errors)
 
   return (
-    <div>
+    <div style={{ maxWidth: '800px', margin: 'auto' }}>
       <h1>Login</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Username</label>
-        <input type="type" id="username" name="username" ref={register} />
+        <input type='type' id='username' name='username' ref={register} />
         <label>Password</label>
-        <input type="password" id="password" name="password" ref={register} />
-        <input type="submit" />
+        <input type='password' id='password' name='password' ref={register} />
+        <input type='submit' />
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
