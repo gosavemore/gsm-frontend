0365e012a7b4a0eb86fbd44f7c87a2a7e5000a22
Sean Doyle <sdoylelambda@gmail.com>
2020-10-27 13:19:52 -0400

    Navbar setup with links for register, sign in, search, home, tracking, and cart.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Route } from 'react-router-dom'
 import './App.css'
 import Login from './components/Login'
 import Register from './components/Register'
@@ -6,8 +7,10 @@ import ProductList from './components/ProductList'
 import AddProduct from './components/AddProduct'
 import EditProduct from './components/EditProduct'
 import GetProduct from './components/GetProduct'
+import Homepage from './components/Homepage'
 import Cart from './components/Cart'
 import GetCart from './components/GetCart'
+import NavBar from './components/NavBar'
 import { useDispatch } from 'react-redux'
 import { getProducts } from './redux/actions/productsActions'
 
@@ -18,20 +21,21 @@ function App() {
     dispatch(getProducts())
   }, [dispatch])
 
+  // FIX SEARCH BAR LINE 32 COMPONENT={} and tracking
+
   return (
     <div className='App'>
-      <h1>Go Save More</h1>
-      <div>
-        <Login />
-        <br></br>
-        <Register />
-        <ProductList />
-        <AddProduct />
-        <EditProduct />
-        <GetProduct />
-        <Cart />
-        <GetCart />
-      </div>
+      <header className='App-header'>
+        <NavBar />
+        <div>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/searchBar' component={GetProduct} />
+          <Route exact='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+          <Route exact='/tracking' component={Homepage} />
+          <Route exact path='/cart' component={GetCart} />
+        </div>
+      </header>
     </div>
   )
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <nav>
+        <div>
+          <Link className='navLink home' to='/'>
+            Home
+          </Link>
+        </div>
+        <div>
+          <Link className='navLink searchBar' to='/searchBar'>
+            MAKE THIS A SEARCH BAR
+          </Link>
+        </div>
+        <div>
+          <Link className='navLink register' to='/register'>
+            Register New User
+          </Link>
+        </div>
+        <div>
+          <Link className='navLink login' to='/login'>
+            Login Existing User
+          </Link>
+          <Link className='navLink track' to='/tracking'>
+            Track Order
+          </Link>
+          <Link className='navLink cart' to='/cart'>
+            Cart
+          </Link>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
