e6f31920dc276090f50a3afe10e32c0ba769033b
Sean Doyle <sdoylelambda@gmail.com>
2020-10-28 14:37:00 -0400

    All pages have responsive designs. Buttons and forms all match. Admin add product added to navbar and displays component.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,54 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div className='navbar'>
-      <nav>
+    <nav>
+      <div className='navbar' class='nav-wrapper'>
+        {/* <div class='nav-wrapper'>
+          <a href='#' class='brand-logo'>
+            GoSaveMore
+          </a>
+          <ul id='nav-mobile' class='right hide-on-med-and-down'>
+            <li>
+              <Link className='navLink home' to='/'>
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link className='navLink searchBar' to='/searchBar'>
+                MAKE THIS A SEARCH BAR
+              </Link>
+            </li>
+            <li>
+              <Link className='navLink register' to='/register'>
+                Register New User
+              </Link>
+            </li>
+            <li>
+              <Link className='navLink login' to='/login'>
+                Login Existing User
+              </Link>
+            </li>
+            <li>
+              <Link className='navLink track' to='/tracking'>
+                Track Order
+              </Link>
+            </li>
+            <li>
+              <Link className='navLink cart' to='/cart'>
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link className='navLink addProduct' to='/admin/addProducts'>
+                Admin Add Products
+              </Link>
+            </li>
+          </ul>
+        </div> */}
         <ul id='nav' class='right  blue-text text-darken-2'>
           <li>
-            <Link className='navLink home' to='/home'>
+            <Link className='navLink home' to='/'>
               Home
             </Link>
           </li>
@@ -37,9 +80,14 @@ const NavBar = () => {
               Cart
             </Link>
           </li>
+          <li>
+            <Link className='navLink addProduct' to='/admin/addProducts'>
+              Admin Add Products
+            </Link>
+          </li>
         </ul>
-      </nav>
-    </div>
+      </div>
+    </nav>
   )
 }
 
--- src/components/Tracking.js
+++ src/components/Tracking.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Tracking = () => {
-  return <h1> Tracking </h1>
+  return (
+    <div style={{ maxWidth: '800px', margin: 'auto', padding: '20px' }}>
+      <h1> Tracking </h1>
+    </div>
+  )
 }
 
 export default Tracking
