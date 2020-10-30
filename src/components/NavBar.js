6df68fb8009755b99145b334b82cd5affdd078d0
Sean Doyle <sdoylelambda@gmail.com>
2020-10-30 12:03:53 -0400

    Hamburger menu displays nav.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,6 +26,7 @@ const NavBar = ({ placeholder, handleChange }) => {
   }, [state, user]);
 
   return (
+     <div>
     <nav>
       <div className="navbar">
       <div class="nav-wrapper">
@@ -83,6 +84,67 @@ const NavBar = ({ placeholder, handleChange }) => {
         </div>
       </div>
     </nav>
+
+    
+
+  <ul class="sidenav sidenav-fixed" id="hamburger">
+      <div className="navbar">
+      <div class="nav-wrapper">
+        {/* <ul class="right hide-on-med-and-down"> */}
+        <>
+          <Link style={{ textAlign: "center" }} className="navLink home" to="/">
+            <img
+              className="navLink image"
+              src={GoSaveMoreLogoHeader}
+              style={{ padding: "10px" }}
+            ></img>
+          </Link>
+        </>
+        <ul id="nav" class="right hide-on-med-and-down">
+          <li>
+            <SearchBar placeholder={placeholder} handleChange={handleChange} />
+          </li>
+          <li>
+            <Link className="navLink cart" to="/cart">
+              Cart
+            </Link>
+          </li>
+          {state.isAdmin ? (
+            <>
+              <li>
+                <Link className="navLink addProduct" to="/admin/addProducts">
+                  Admin Add Products
+                </Link>
+              </li>
+            </>
+          ) : null}
+          {state.isAuth ? (
+            <>
+              <li>
+                <Link className="navLink track" to="/tracking">
+                  Orders
+                </Link>
+              </li>
+              <li>
+                <Link className="signout" onClick={handleLogOut} to="/tracking">
+                  Logout
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link className="navLink login" to="/login">
+                  Login
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+        </div>
+      </div>
+  </ul>
+          </div>
   );
 };
 
