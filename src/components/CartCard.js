5c248ee4ae6b7438f262e30ba277134a94743e49
denmercs <denmercs@gmail.com>
2020-10-30 21:57:00 -0500

    scss added in product list, align nodes into center including button

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Card, CardTitle } from "react-materialize";
+import "./CartCard.scss";
 
 const CartCard = (props) => {
   const {
@@ -7,19 +9,23 @@ const CartCard = (props) => {
     shortDescription,
     description,
     quantity,
+    image,
   } = props.product;
 
   return (
-    <div class="col s12 m3">
-      <div class="card">
-        <div class="card-content">
-          <h3>{productName} </h3>
-          <p>${price}.00 </p>
-          <p>{shortDescription} </p>
-          <p>{description} </p>
-          <p>Qty: {quantity}</p>
-        </div>
-      </div>
+    <div className="cart-cards">
+      <Card
+        header={
+          <CardTitle image={image} className="product-name">
+            {productName}
+          </CardTitle>
+        }
+      >
+        <p>${price}.00 </p>
+        <p>{shortDescription} </p>
+        <p>{description} </p>
+        <p>Qty: {quantity}</p>
+      </Card>
     </div>
   );
 };
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,45 +1,31 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { addToCart } from '../redux/actions/cartActions'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actions/cartActions";
+import { Card, CardTitle, Button } from "react-materialize";
+import "./ProductCard.scss";
 
 const ProductCard = (props) => {
-  const dispatch = useDispatch()
-  let product = props.product
+  const dispatch = useDispatch();
+  let product = props.product;
 
-  const {
-    productName,
-    price,
-    shortDescription,
-    description,
-    stock,
-  } = props.product
+  const { productName, image } = props.product;
 
   const handleClick = () => {
-    dispatch(addToCart(product, 'testing'))
-  }
+    dispatch(addToCart(product, "testing"));
+  };
 
   return (
     // <div class='row'>
-    <div class='col s12 m3'>
-      <div class='card'>
-        <div class='card-content'>
-          <h3>{productName} </h3>
-          <p>${price}.00 </p>
-          <p>{shortDescription} </p>
-          <p>{description} </p>
-          <p>Only {stock} left in stock!</p>
-          <button
-            class='btn waves-effect waves-light'
-            type='submit'
-            onClick={handleClick}
-          >
-            Add To Cart
-          </button>
-        </div>
-      </div>
-    </div>
+    <Card className="product">
+      <CardTitle image={image} className="product-name">
+        {productName}
+      </CardTitle>
+      <Button waves="light" onClick={handleClick} className="product-button">
+        Add To Cart
+      </Button>
+    </Card>
     // </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
