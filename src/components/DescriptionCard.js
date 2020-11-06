34c4f5aabbb1f909504de36463ddfcf7add2a7ce
Sean Doyle <sdoylelambda@gmail.com>
2020-11-06 11:49:41 -0500

    Description for products on button click on card.

---
+++ src/components/DescriptionCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DescriptionCart = (props) => {
+  return (
+    <div>
+      <h1>D Cart</h1>
+    </div>
+  )
+}
+
+export default DescriptionCart
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,34 +1,53 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/actions/cartActions";
-import { Card, CardTitle, Button } from "react-materialize";
-import "./ProductCard.scss";
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../redux/actions/cartActions'
+import { Card, CardTitle, CardDescription, Button } from 'react-materialize'
+import './ProductCard.scss'
+import DescriptionCard from './DescriptionCard'
 
 const ProductCard = (props) => {
-  const dispatch = useDispatch();
-  let product = props.product;
-
-  const { productName, price, image } = props.product;
+  const dispatch = useDispatch()
+  let product = props.product
+  console.log('props.product===>', props.product)
+  const {
+    productName,
+    price,
+    image,
+    shortDescription,
+    description,
+  } = props.product
 
   const handleClick = () => {
-    dispatch(addToCart(product));
-  };
+    dispatch(addToCart(product))
+  }
+
+  const handleClickInfo = () => {
+    dispatch(addToCart(product)) //TBC link to description cart.
+  }
 
   return (
-    <Card className="product">
-      <CardTitle image={image} className="product-name">
+    <Card className='product'>
+      <CardTitle image={image} className='product-name'>
         {productName}
       </CardTitle>
+      <div className='product-shortDescription'>{shortDescription}</div>
+      <Button
+        waves='light'
+        onClick={handleClickInfo}
+        className='product-button string'
+      >
+        Info
+      </Button>
       <p>${price}</p>
       <Button
-        waves="light"
+        waves='light'
         onClick={handleClick}
-        className="product-button string"
+        className='product-button string'
       >
         Add
       </Button>
     </Card>
-  );
-};
+  )
+}
 
-export default ProductCard;
+export default ProductCard
