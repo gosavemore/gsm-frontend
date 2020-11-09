4ed7c41f86b61e64053b82505607e6e3db1407b2
Sean Doyle <sdoylelambda@gmail.com>
2020-11-09 14:51:21 -0500

    Button on product cards links to product info page.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,67 @@
-import React, { useState, useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.scss";
+import React, { useState, useEffect } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import './App.scss'
 
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AddProduct from "./components/AddProduct";
-import Homepage from "./pages/Homepage";
-import NavBar from "./pages/NavBar";
-import Footer from "./pages/Footer";
-import Banner from "./components/Banner";
-import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "./redux/actions/productsActions";
-import PrivateRoute from "./redux/util/privateRoute";
-import CartList from "./components/CartList";
-import Orders from "./components/Orders";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import AddProduct from './components/AddProduct'
+import Homepage from './pages/Homepage'
+import NavBar from './pages/NavBar'
+import ProductInfo from './pages/ProductInfo'
+import Footer from './pages/Footer'
+import Banner from './components/Banner'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProducts } from './redux/actions/productsActions'
+import PrivateRoute from './redux/util/privateRoute'
+import CartList from './components/CartList'
+import Orders from './components/Orders'
+import ProductList from './components/ProductList'
 
 function App() {
   const [search, setSearch] = useState({
-    searchField: "",
-  });
+    searchField: '',
+  })
 
-  const products = useSelector((state) => state.products.products);
-  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.products)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
+    dispatch(getProducts())
+  }, [dispatch])
 
   const handleChange = (e) => {
-    setSearch({ searchField: e.target.value });
-  };
+    setSearch({ searchField: e.target.value })
+  }
 
   // search product if exists
-  let filteredProduct;
+  let filteredProduct
 
   if (search.searchField) {
     filteredProduct = products.filter((product) =>
       product.productName.toLowerCase().startsWith(search.searchField)
-    );
+    )
   } else {
-    filteredProduct = products;
+    filteredProduct = products
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Banner />
-      <header className="App-header">
-        <NavBar placeholder={"Search Product"} handleChange={handleChange} />
+      <header className='App-header'>
+        <NavBar placeholder={'Search Product'} handleChange={handleChange} />
         <div>
           <Switch>
             <Route
               exact
-              path="/"
+              path='/'
               component={() => <Homepage filteredProduct={filteredProduct} />}
             />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/cart" component={CartList} />
-            <Route exact path="/admin/addProducts" component={AddProduct} />
-            <PrivateRoute exact path="/orders" component={Orders} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/cart' component={CartList} />
+            <Route exact path='/admin/addProducts' component={AddProduct} />
+            <Route exact path='/product' component={ProductInfo} />
+            <PrivateRoute exact path='/orders' component={Orders} />
           </Switch>
         </div>
       </header>
@@ -68,7 +71,7 @@ function App() {
         </div>
       </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
