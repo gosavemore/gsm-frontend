7b4039ec53fcf74fdd6f97f23c2ed1ed5aa1fbe5
denmercs <denmercs@gmail.com>
2020-11-10 15:26:52 -0600

    added styling in productinfo and integrate the button button to cart store

--- src/pages/ProductInfo.js
+++ src/pages/ProductInfo.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProduct } from "../redux/actions/productsActions";
+import { MediaBox, Button } from "react-materialize";
+import "./ProductInfo.scss";
+import { addToCart } from "../redux/actions/cartActions";
 
 const ProductInfo = (props) => {
   let id = props.match.params.id;
@@ -8,19 +11,38 @@ const ProductInfo = (props) => {
   const dispatch = useDispatch();
   const item = useSelector((state) => state.products.item);
 
+  const handleClick = () => {
+    dispatch(addToCart(item));
+    console.log("this is the product", props);
+  };
+
   useEffect(() => {
     dispatch(getProduct(id));
   }, []);
 
   return (
     <>
-      <div className="ProductInfo">
-        <h3>{item.productName}</h3>
-        <h4>${item.price}</h4>
-        <p>{item.shortDescription}</p>
-        <p>Description: {item.description}</p>
-        <p>Stock: {item.stock}</p>
-        <p>Ratings: {item.ratings}</p>
+      <div className="product-info-container">
+        <MediaBox>
+          <img alt={item.productName} src={item.image} />
+        </MediaBox>
+        <div className="product-info-details">
+          <h3>{item.productName}</h3>
+          <h4>${item.price}</h4>
+          <h4>Description: </h4>
+          <p>{item.description}</p>
+          <h4>Stocks: </h4>
+          <p>{item.stock}</p>
+          <h4>Ratings: </h4>
+          <p>{item.ratings}</p>
+          <Button
+            waves="light"
+            className="product-info-button"
+            onClick={handleClick}
+          >
+            Add
+          </Button>
+        </div>
       </div>
     </>
   );
