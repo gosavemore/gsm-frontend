81063fd2c377dc4bb92e5adc577ee2b48f5dd812
denmercs <denmercs@gmail.com>
2020-11-09 22:51:17 -0600

    added individual information once click in page list

--- src/pages/ProductInfo.js
+++ src/pages/ProductInfo.js
@@ -1,15 +1,25 @@
-import React from 'react'
-// import { getProduct } from './'
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getProduct } from "../redux/actions/productsActions";
 
-const ProductInfo = (productName) => {
-  //   let product = props.product
-  // const { productName, price, image } = props.product
-  console.log('props here =====>', productName)
+const ProductInfo = (props) => {
+  let id = props.match.params.id;
+
+  const dispatch = useDispatch();
+  const item = useSelector((state) => state.products.item);
+
+  useEffect(() => {
+    dispatch(getProduct(id));
+  }, []);
+
   return (
-    <div className='productInfo'>
-      <p>p info</p>
-    </div>
-  )
-}
+    <>
+      <div className="productInfo">{item.productName}</div>
+      <div className="productInfo">{item.price}</div>
+      <div className="productInfo">{item.brand}</div>
+      <div className="productInfo">{item.ratings}</div>
+    </>
+  );
+};
 
-export default ProductInfo
+export default ProductInfo;
