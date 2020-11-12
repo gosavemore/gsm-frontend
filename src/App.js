c0aba456bd84fd556576016080c4be51aedfd6f0
Sean Doyle <sdoylelambda@gmail.com>
2020-11-12 12:45:27 -0500

    Checkout button links to checkout component.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,73 @@
-import React, { useState, useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.scss";
+import React, { useState, useEffect } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import './App.scss'
 
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AddProduct from "./components/AddProduct";
-import Homepage from "./pages/Homepage";
-import NavBar from "./pages/NavBar";
-import ProductInfo from "./pages/ProductInfo";
-import Footer from "./pages/Footer";
-import Banner from "./components/Banner";
-import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "./redux/actions/productsActions";
-import PrivateRoute from "./redux/util/privateRoute";
-import CartList from "./components/CartList";
-import Orders from "./components/Orders";
-import { getCart } from "./redux/actions/cartActions";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import AddProduct from './components/AddProduct'
+import Homepage from './pages/Homepage'
+import NavBar from './pages/NavBar'
+import ProductInfo from './pages/ProductInfo'
+import Checkout from './pages/Checkout'
+import Footer from './pages/Footer'
+import Banner from './components/Banner'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProducts } from './redux/actions/productsActions'
+import PrivateRoute from './redux/util/privateRoute'
+import CartList from './components/CartList'
+import Orders from './components/Orders'
+import { getCart } from './redux/actions/cartActions'
 
 function App() {
   const [search, setSearch] = useState({
-    searchField: "",
-  });
+    searchField: '',
+  })
 
-  const products = useSelector((state) => state.products.products);
-  const auth = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.products)
+  const auth = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(getProducts());
-    if(auth.isAuth) {
-      dispatch(getCart(auth.user.id));
+    dispatch(getProducts())
+    if (auth.isAuth) {
+      dispatch(getCart(auth.user.id))
     }
-  }, [dispatch, auth]);
-
+  }, [dispatch, auth])
 
   const handleChange = (e) => {
-    setSearch({ searchField: e.target.value });
-  };
+    setSearch({ searchField: e.target.value })
+  }
 
   // search product if exists
-  let filteredProduct;
+  let filteredProduct
 
   if (search.searchField) {
     filteredProduct = products.filter((product) =>
       product.productName.toLowerCase().startsWith(search.searchField)
-    );
+    )
   } else {
-    filteredProduct = products;
+    filteredProduct = products
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Banner />
-      <header className="App-header">
-        <NavBar placeholder={"Search Product"} handleChange={handleChange} />
+      <header className='App-header'>
+        <NavBar placeholder={'Search Product'} handleChange={handleChange} />
         <div>
           <Switch>
             <Route
               exact
-              path="/"
+              path='/'
               component={() => <Homepage filteredProduct={filteredProduct} />}
             />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/cart" component={CartList} />
-            <Route exact path="/admin/addProducts" component={AddProduct} />
-            <Route exact path="/product/:id" component={ProductInfo} />
-            <PrivateRoute exact path="/orders" component={Orders} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/cart' component={CartList} />
+            <Route exact path='/admin/addProducts' component={AddProduct} />
+            <Route exact path='/product/:id' component={ProductInfo} />
+            <Route exact path='/checkout' component={Checkout} />
+            <PrivateRoute exact path='/orders' component={Orders} />
           </Switch>
         </div>
       </header>
@@ -76,7 +77,7 @@ function App() {
         </div>
       </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/CartList.js
+++ src/components/CartList.js
@@ -1,61 +1,61 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import CartCard from "./CartCard";
-import { Button } from "react-materialize";
-import "./CartList.scss";
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import CartCard from './CartCard'
+import { Button } from 'react-materialize'
+import './CartList.scss'
 
 const CartList = () => {
-  const cartData = useSelector((state) => state.cart);
-  
-  const dispatch = useDispatch();
+  const cartData = useSelector((state) => state.cart)
+
+  const dispatch = useDispatch()
 
   const [products, setProducts] = useState({
     totalItems: 0,
     totalPrice: 0,
-  });
+  })
 
   useEffect(() => {
     setProducts({
       totalItems: cartData.totalItems,
       totalPrice: cartData.totalPrice,
-    });
-  }, [cartData]);
-
-  
+    })
+  }, [cartData])
 
   return (
-    <div className="cart-page">
-      <div className="cart-ring-up">
+    <div className='cart-page'>
+      <div className='cart-ring-up'>
         <h3> Total Items</h3>
         <p>{products.totalItems}</p>
         <h3> Total Price</h3>
         <p>${products.totalPrice}</p>
-        <Button>Checkout</Button>
+        <Link to={'/checkout'}>
+          <Button>Checkout</Button>
+        </Link>
       </div>
-      <div className="cart-list">
-        <div className="cart-list-product">
+      <div className='cart-list'>
+        <div className='cart-list-product'>
           <h3>Your Shopping Cart</h3>
-          { cartData.items.map((product) => {
-              if (product.quantity !== 0) {
-                return (
-                  <CartCard
-                    key={product.id}
-                    product={product}
-                    setProducts={setProducts}
-                    totalItems={products.totalItems}
-                    totalPrice={products.totalPrice}
-                  />
-                );
-              }
-            }) 
-          }
+          {cartData.items.map((product) => {
+            if (product.quantity !== 0) {
+              return (
+                <CartCard
+                  key={product.id}
+                  product={product}
+                  setProducts={setProducts}
+                  totalItems={products.totalItems}
+                  totalPrice={products.totalPrice}
+                />
+              )
+            }
+          })}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CartList;
+export default CartList
 
 // FOR FUN
 // MAKE CART VERY SECURE USING SHA256
